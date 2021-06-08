e3713507a14f09403f6f4c733d0a2667b22afb43
Ayan Biswas <ayanbiswas184@gmail.com>
2021-06-08 21:20:37 +0530

    update: login logic done , custom notice handling to be done

--- app/controllers/lti_controller.rb
+++ app/controllers/lti_controller.rb
@@ -1,14 +1,15 @@
 class LtiController < ApplicationController
   before_action :set_group, only: %i[launch]
   after_action :allow_iframe, only: %i[launch]
+  before_action :set_lti_params, only: %i[launch]
   
   def launch
     #If set, then hide the header and footer
     session[:isLTI]=true
-    
     require 'oauth/request_proxy/action_controller_request'
     
     if @group.present?
+      #if there is a valid group based for the lti_token_key
       @provider = IMS::LTI::ToolProvider.new(
         params[:oauth_consumer_key], ## moodle_key
         @group.lti_token, ## the group's lti_token
@@ -21,28 +22,31 @@ class LtiController < ApplicationController
       end
 
       @@launch_params=params;
-      email_from_lms = params[:lis_person_contact_email_primary] ## caches the email from the LMS
-      lms_type = params[:tool_consumer_info_product_family_code] ## caches type of lms like moodle/canvas
-      course_title_from_lms = params[:context_title] ## caches the course titile from lms
-
-      # implement the logic of fetch group by token and auth user without passsword
       # get group from token  and we have to make a permanent toekn per group onClick
       # if user is member of group then sign_in(:user) else send an email
-      user = User.find_by(email: email_from_lms)
+      user = User.find_by(email: @email_from_lms)
+
       if user.present?
-        sign_in(user)
-        lms_auth_success_notice = 'Logged in as '+email_from_lms+' via '+lms_type+' for course '+course_title_from_lms
-        redirect_to group_path(@group), notice: lms_auth_success_notice
+        user_in_group = GroupMember.find_by(user_id:user.id,group_id:@group.id)
+
+        if user_in_group.present? || user.id === @group.mentor_id 
+          # if user is a member or mentor of the group then allow authentication
+          sign_in(user)
+          lms_auth_success_notice = 'Logged in as '+@email_from_lms+' via '+@lms_type+' for course '+@course_title_from_lms
+          redirect_to group_path(@group), notice: lms_auth_success_notice # if auth_success send to group page
+        else # if the user is not a member of the group then we have to send mail
+          render :launch_error, status: 401 #notice has to be given
+          return
+        end
+      else
+        # if there is no such user in circuitverse, then we have to send mail
+        render :launch_error, status: 401 #notice has to be given
+        return
       end
-      
-      # if auth_success send to group page
-      ##checking need to be done if the user is part of the group or not
-      #redirect_to "/groups/#{@group.id}"  ## ideally to group page
-      
-      
-      
-      
-      
+    else
+      #if there is no valid group present for the lti_token_key
+      render :launch_error, status: 401 #notice has to be given
+      return
     end
   end
 
@@ -53,6 +57,12 @@ class LtiController < ApplicationController
   private
     def set_group
       ## query db and check moodle_key is equal to group where @group.lti_token_key == moodle_key
-      @group = Group.select(:id,:lti_token).find_by(lti_token_key: params[:oauth_consumer_key])
+      @group = Group.select(:id,:mentor_id,:lti_token).find_by(lti_token_key: params[:oauth_consumer_key])
+    end
+    
+    def set_lti_params
+      @email_from_lms = params[:lis_person_contact_email_primary] # the email from the LMS
+      @lms_type = params[:tool_consumer_info_product_family_code] # type of lms like moodle/canvas
+      @course_title_from_lms = params[:context_title] # the course titile from lms
     end
 end
